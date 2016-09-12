15d4b22de73eac28333d528eccd7d71a19efb8f7
Dylan McKay <dylanmckay34@gmail.com>
2016-09-12 18:50:51 +1200

    Allow reading/writing VecDeque objects

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -10,6 +10,7 @@ pub mod map;
 pub mod string;
 pub mod tuple;
 pub mod option;
+pub mod vec_deque;
 #[cfg(feature = "uuid")]
 pub mod uuid;
 
@@ -27,6 +28,11 @@ pub trait Type : Clone + fmt::Debug
 
     /// Writes a type to a stream.
     fn write(&self, write: &mut Write) -> Result<(), ::Error>;
+
+    fn from_raw_bytes(bytes: &[u8]) -> Result<Self, ::Error> {
+        let mut buffer = ::std::io::Cursor::new(bytes);
+        Self::read(&mut buffer)
+    }
 
     fn raw_bytes(&self) -> Result<Vec<u8>, ::Error> {
         let mut buffer = io::Cursor::new(Vec::new());
