61a804e5af0ba5aee25402e74de279a976ab2c48
Dylan McKay <me@dylanmckay.io>
2018-06-11 23:18:46 +1200

    Support variant names as protocol discriminators
    This modifies protocol-derive so that we can automatically derive enum
    Parcel implementations that serialise the variant name rather than
    numerical index as the descriminator.
    
    This is much more future-proof.

--- protocol-derive-tests/src/enums.rs
+++ protocol-derive-tests/src/enums.rs
@@ -1,48 +1,83 @@
-#[allow(unused_imports)]
-use protocol::Parcel;
+#[cfg(test)]
+mod string_discriminants {
+    #[allow(unused_imports)]
+    use protocol::Parcel;
 
-#[derive(Protocol, Debug, PartialEq, Eq)]
-pub enum BoatKind {
-    Speedboat { warp_speed_enabled: bool },
-    Dingy(u8, u8),
-    Fart,
+    #[derive(Protocol, Debug, PartialEq)]
+    #[protocol_discriminant = "string"]
+    pub enum Axis { X, Y, Z, Other(String), Bimp { val: u64 } }
+
+    fn verify_read_back<P: Parcel + ::std::fmt::Debug + ::std::cmp::PartialEq>(parcel: P) {
+        let read_back = P::from_raw_bytes(&parcel.raw_bytes().unwrap()[..]).unwrap();
+        assert_eq!(parcel, read_back);
+    }
+
+    #[test]
+    fn variant_names_are_discriminators() {
+        assert_eq!(vec![0, 0, 0, 1, 'X' as _], Axis::X.raw_bytes().unwrap());
+        assert_eq!(vec![0, 0, 0, 5, 'O' as _, 't' as _, 'h' as _, 'e' as _, 'r' as _,
+                        0, 0, 0, 4, 'r' as _, 'o' as _, 'l' as _, 'l' as _],
+                   Axis::Other("roll".to_owned()).raw_bytes().unwrap());
+    }
+
+    #[test]
+    fn can_write_and_read_back() {
+        verify_read_back(Axis::Other("boop".to_owned()));
+        verify_read_back(Axis::X);
+        verify_read_back(Axis::Y);
+        verify_read_back(Axis::Bimp { val: 77 });
+    }
 }
 
-#[test]
-fn named_fields_are_correctly_written() {
-    assert_eq!(vec![0, 0, 0, 1, 1], BoatKind::Speedboat {
-        warp_speed_enabled: true,
-    }.raw_bytes().unwrap());
-}
+#[cfg(test)]
+mod integer_discriminants {
+    #[allow(unused_imports)]
+    use protocol::Parcel;
 
-#[test]
-fn unnamed_fields_are_correctly_written() {
-    assert_eq!(vec![0, 0, 0, 2, // discriminator
-                    0xf1, 0xed], BoatKind::Dingy(0xf1, 0xed).raw_bytes().unwrap());
-}
+    #[derive(Protocol, Debug, PartialEq, Eq)]
+    #[protocol_discriminant = "integer"]
+    pub enum BoatKind {
+        Speedboat { warp_speed_enabled: bool },
+        Dingy(u8, u8),
+        Fart,
+    }
 
-#[test]
-fn unit_variants_are_correctly_written() {
-    assert_eq!(vec![0, 0, 0, 3], // discriminator
-               BoatKind::Fart.raw_bytes().unwrap());
-}
+    #[test]
+    fn named_fields_are_correctly_written() {
+        assert_eq!(vec![0, 0, 0, 1, 1], BoatKind::Speedboat {
+            warp_speed_enabled: true,
+        }.raw_bytes().unwrap());
+    }
 
-#[test]
-fn named_fields_are_correctly_read() {
-    assert_eq!(BoatKind::Speedboat {
-        warp_speed_enabled: true,
-    }, BoatKind::from_raw_bytes(&[0, 0, 0, 1, 1]).unwrap());
-}
+    #[test]
+    fn unnamed_fields_are_correctly_written() {
+        assert_eq!(vec![0, 0, 0, 2, // discriminator
+                        0xf1, 0xed], BoatKind::Dingy(0xf1, 0xed).raw_bytes().unwrap());
+    }
 
-#[test]
-fn unnamed_fields_are_correctly_read() {
-    assert_eq!(BoatKind::Dingy(99, 78),
-               BoatKind::from_raw_bytes(&[0, 0, 0, 2, 99, 78]).unwrap());
-}
+    #[test]
+    fn unit_variants_are_correctly_written() {
+        assert_eq!(vec![0, 0, 0, 3], // discriminator
+                   BoatKind::Fart.raw_bytes().unwrap());
+    }
 
-#[test]
-fn unit_variants_are_correctly_read() {
-    assert_eq!(BoatKind::Fart,
-               BoatKind::from_raw_bytes(&[0, 0, 0, 3]).unwrap());
+    #[test]
+    fn named_fields_are_correctly_read() {
+        assert_eq!(BoatKind::Speedboat {
+            warp_speed_enabled: true,
+        }, BoatKind::from_raw_bytes(&[0, 0, 0, 1, 1]).unwrap());
+    }
+
+    #[test]
+    fn unnamed_fields_are_correctly_read() {
+        assert_eq!(BoatKind::Dingy(99, 78),
+                   BoatKind::from_raw_bytes(&[0, 0, 0, 2, 99, 78]).unwrap());
+    }
+
+    #[test]
+    fn unit_variants_are_correctly_read() {
+        assert_eq!(BoatKind::Fart,
+                   BoatKind::from_raw_bytes(&[0, 0, 0, 3]).unwrap());
+    }
 }
 
--- protocol-derive/src/lib.rs
+++ protocol-derive/src/lib.rs
@@ -1,15 +1,16 @@
 #![recursion_limit = "128"]
 
 extern crate proc_macro;
+extern crate proc_macro2;
 extern crate syn;
 #[macro_use]
 extern crate quote;
 
+mod format;
+
 use proc_macro::TokenStream;
 
-type Discriminator = u32;
-
-#[proc_macro_derive(Protocol)]
+#[proc_macro_derive(Protocol, attributes(protocol_discriminant))]
 pub fn protocol(input: TokenStream) -> TokenStream {
     // Parse the string representation
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
@@ -22,7 +23,7 @@ pub fn protocol(input: TokenStream) -> TokenStream {
 }
 
 // The `Parcel` trait is used for data that can be sent/received.
-fn impl_parcel(ast: &syn::DeriveInput) -> quote::Tokens {
+fn impl_parcel(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
     match ast.data {
         syn::Data::Struct(ref s) => impl_parcel_for_struct(ast, s),
         syn::Data::Enum(ref e) => impl_parcel_for_enum(ast, e),
@@ -31,14 +32,14 @@ fn impl_parcel(ast: &syn::DeriveInput) -> quote::Tokens {
 }
 
 fn impl_parcel_for_struct(ast: &syn::DeriveInput,
-                          strukt: &syn::DataStruct) -> quote::Tokens {
+                          strukt: &syn::DataStruct) -> proc_macro2::TokenStream {
     let strukt_name = &ast.ident;
-    let anon_const_name = syn::Ident::from(format!("__IMPL_PARCEL_FOR_{}", strukt_name.to_owned()));
+    let anon_const_name = syn::Ident::new(&format!("__IMPL_PARCEL_FOR_{}", strukt_name.to_owned()), proc_macro2::Span::call_site());
 
     match strukt.fields {
         syn::Fields::Named(ref fields_named) => {
             let field_names: Vec<_> = fields_named.named.iter().map(|field| {
-                field.ident
+                &field.ident
             }).collect();
             let field_names = &field_names[..];
 
@@ -125,24 +126,44 @@ fn impl_parcel_for_struct(ast: &syn::DeriveInput,
     }
 }
 
+fn protocol_discriminant<F: format::Format>(ast: &syn::DeriveInput) -> Option<F> {
+    ast.attrs.iter().filter_map(|attr| match attr.interpret_meta() {
+        Some(syn::Meta::NameValue(syn::MetaNameValue { ident, lit, .. })) => {
+            if ident == syn::Ident::new("protocol_discriminant", proc_macro2::Span::call_site()) {
+                match lit {
+                    syn::Lit::Str(s) => {
+                        match F::from_str(&s.value()) {
+                            Ok(f) => Some(f),
+                            Err(..) => None,
+                        }
+                    },
+                    _ => panic!("unexpected type"),
+                }
+            } else {
+                None
+            }
+        },
+        _ => None,
+    }).next()
+}
+
 fn impl_parcel_for_enum(ast: &syn::DeriveInput,
-                        e: &syn::DataEnum) -> quote::Tokens {
-    let enum_name = ast.ident;
-    let anon_const_name = syn::Ident::from(format!("__IMPL_PARCEL_FOR_{}", ast.ident));
+                        e: &syn::DataEnum) -> proc_macro2::TokenStream {
+    let enum_name = &ast.ident;
+    let anon_const_name = syn::Ident::new(&format!("__IMPL_PARCEL_FOR_{}", ast.ident), proc_macro2::Span::call_site());
 
-    let variant_writers = e.variants.iter().enumerate().map(|(variant_index, variant)| {
-        // Reserve discriminator 0.
-        let discriminator = (variant_index as Discriminator) + 1;
-        let variant_name = variant.ident;
+    let format = protocol_discriminant::<format::Enum>(ast).unwrap_or(format::Enum::IntegerDiscriminator);
 
-        let write_discriminator = quote! {
-            protocol::Parcel::write(&#discriminator, __io_writer)?;
-        };
+    let variant_writers = e.variants.iter().map(|variant| {
+        let variant_name = &variant.ident;
+
+        let discriminator = format.discriminator(e, variant);
+        let write_discriminator = quote! { protocol::Parcel::write(&#discriminator, __io_writer)?; };
 
         match variant.fields {
             syn::Fields::Named(ref fields_named) => {
-                let field_names = fields_named.named.iter().map(|f| f.ident);
-                let field_name_refs = fields_named.named.iter().map(|f| f.ident).map(|n| quote! { ref #n });
+                let field_names = fields_named.named.iter().map(|f| &f.ident);
+                let field_name_refs = fields_named.named.iter().map(|f| &f.ident).map(|n| quote! { ref #n });
 
                 quote! {
                     #enum_name :: #variant_name { #( #field_name_refs ),* } => {
@@ -154,7 +175,7 @@ fn impl_parcel_for_enum(ast: &syn::DeriveInput,
             },
             syn::Fields::Unnamed(ref fields_unnamed) => {
                 let binding_names: Vec<_> = (0..fields_unnamed.unnamed.len()).into_iter()
-                    .map(|i| syn::Ident::from(format!("field_{}", i)))
+                    .map(|i| syn::Ident::new(&format!("field_{}", i), proc_macro2::Span::call_site()))
                     .collect();
 
                 let field_refs: Vec<_> = binding_names.iter().map(|i| quote! { ref #i }).collect();
@@ -170,20 +191,19 @@ fn impl_parcel_for_enum(ast: &syn::DeriveInput,
                 quote!{
                     #enum_name :: #variant_name => {
                         #write_discriminator;
-                    },
+                    }
                 }
             },
         }
     });
 
-    let variant_readers = e.variants.iter().enumerate().map(|(variant_index, variant)| {
-        // Reserve discriminator 0.
-        let discriminator = (variant_index as Discriminator) + 1;
-        let variant_name = variant.ident;
+    let variant_readers = e.variants.iter().map(|ref variant| {
+        let variant_name = &variant.ident;
+        let discriminator = format.discriminator_variant_for_pattern_matching(e, variant);
 
         match variant.fields {
             syn::Fields::Named(ref fields_named) => {
-                let field_names = fields_named.named.iter().map(|f| f.ident);
+                let field_names = fields_named.named.iter().map(|f| &f.ident);
 
                 quote! {
                     #discriminator => Ok(#enum_name :: #variant_name {
@@ -193,7 +213,7 @@ fn impl_parcel_for_enum(ast: &syn::DeriveInput,
             },
             syn::Fields::Unnamed(ref fields_unnamed) => {
                 let binding_names: Vec<_> = (0..fields_unnamed.unnamed.len()).into_iter()
-                    .map(|i| syn::Ident::from(format!("field_{}", i)))
+                    .map(|i| syn::Ident::new(&format!("field_{}", i), proc_macro2::Span::call_site()))
                     .collect();
 
                 let field_readers = binding_names.iter().map(|_| {
@@ -216,6 +236,8 @@ fn impl_parcel_for_enum(ast: &syn::DeriveInput,
         }
     });
 
+    let discriminator_type = format.discriminator_type();
+    let discriminator_for_pattern_matching = format.discriminator_for_pattern_matching();
     quote! {
         #[allow(non_upper_case_globals)]
         const #anon_const_name: () = {
@@ -225,8 +247,8 @@ fn impl_parcel_for_enum(ast: &syn::DeriveInput,
             impl protocol::Parcel for #enum_name {
                 #[allow(unused_variables)]
                 fn read(__io_reader: &mut io::Read) -> Result<Self, protocol::Error> {
-                    let discriminator: u32 = protocol::Parcel::read(__io_reader)?;
-                    match discriminator {
+                    let discriminator: #discriminator_type = protocol::Parcel::read(__io_reader)?;
+                    match #discriminator_for_pattern_matching {
                         #(#variant_readers,)*
                         _ => panic!("unknown discriminator"),
                     }
