58164ee1936fd7e93d220390054efe31fd7634a6
Dylan McKay <me@dylanmckay.io>
2018-11-03 05:11:46 +1300

    Write some documentation

--- protocol/src/parcel.rs
+++ protocol/src/parcel.rs
@@ -2,6 +2,41 @@ use std::io::prelude::*;
 use std::io;
 
 /// A value which can be read and written.
+///
+/// All of the expected standard Rust types implement this.
+///
+/// Types that implement `Parcel` include (not exhaustive):
+///
+///   * The Rust primitive types
+///     * `u8`, `i8`, `u16`, `i16` ... `u64`
+///     * `bool`, `char`
+///     * `f32`, `f64`
+///     * Tuples
+///       * `(T1) where T1: Parcel`
+///       * `(T1, T2) where T1: Parcel, T2: Parcel`
+///       * `(T1, T2, ... Tn) where T1: Parcel, T2: Parcel, ... Tn: Parcel`
+///     * Arrays
+///       * `[T; 0] where T: Parcel`
+///       * `[T; 1] where T: Parcel`
+///       * `[T; 2] where T: Parcel`
+///       * `[T; 32] where T: Parcel`
+///       * `[T; 40] where T: Parcel`
+///       * `[T; 42] where T: Parcel`
+///       * ...
+///       * `[T; 64] where T: Parcel`
+///       * ...
+///       * `[T; 1024] where T: Parcel`
+///     * `String`
+///     * `Option<T: Parcel>`
+///     * `Box<T: Parcel>`
+///     * Smart pointers
+///       * `std::rc::Rc`
+///       * `std::sync::Arc`
+///     * `std::collections`
+///       * `Vec<T: Parcel>`
+///       * `VecDeque<T: Parcel>`
+///       * `HashMap<T: Parcel>`
+///       * `BTreeMap<T: Parcel>`
 pub trait Parcel : Sized
 {
     /// Reads a value from a stream.
@@ -10,6 +45,7 @@ pub trait Parcel : Sized
     /// Writes a value to a stream.
     fn write(&self, write: &mut Write) -> Result<(), ::Error>;
 
+    /// Creates a new parcel
     fn from_raw_bytes(bytes: &[u8]) -> Result<Self, ::Error> {
         let mut buffer = ::std::io::Cursor::new(bytes);
         Self::read(&mut buffer)
