a4cff76156f1ff3ab91256204025d7fb2a965887
Dylan McKay <me@dylanmckay.io>
2018-11-25 12:43:24 +1300

    Fix some bugs in the release script

--- release.sh
+++ release.sh
@@ -2,7 +2,10 @@
 
 DIR=$(dirname $0)
 
-CURRENT_VERSION=$(sed -E 's/^version = "(.*)"/\1/;t;d' "$DIR/protocol/Cargo.toml")
+NEW_VERSION=$1
+if [ -z "$1" ]; then echo "please pass a new version number on the cmdline" && exit 1; fi
+
+PREVIOUS_VERSION=$(sed -E 's/^version = "(.*)"/\1/;t;d' "$DIR/protocol/Cargo.toml")
 
 if ! git diff-index --quiet HEAD --; then
   echo "git worktree has uncomitted changes" 1>&2
@@ -10,12 +13,10 @@ if ! git diff-index --quiet HEAD --; then
 fi
 
 function update_manifest_version {
-  new_version=$1
-  find -name Cargo.toml -not -path "*/target/*" -exec sed -Ei "s/$CURRENT_VERSION/$new_version/g" "{}" \+
-  git commit -am "Bump version to $new_version"
+  find -name Cargo.toml -not -path "*/target/*" -exec sed -Ei "s/$PREVIOUS_VERSION/$NEW_VERSION/g" "{}" \+
+  git commit -am "Bump version to $NEW_VERSION"
 }
 
-if [ -z "$1" ]; then echo "please pass a new version number on the cmdline" && exit 1; fi
 update_manifest_version $1
 
 cd protocol-derive
@@ -24,10 +25,10 @@ cargo publish
 cd ../protocol
 cargo publish
 
-echo tagging $CURRENT_VERSION
+echo tagging $NEW_VERSION
 
-git tag $CURRENT_VERSION
-git push origin master $CURRENT_VERSION
+git tag $NEW_VERSION
+git push origin master $NEW_VERSION
 
 echo "pls update the crate versions in the README if you changed the major or minor version"
 
