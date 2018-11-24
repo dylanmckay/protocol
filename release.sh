6cf3e8b25315cab8881b209f57b0606bbb11a9d2
Dylan McKay <me@dylanmckay.io>
2018-11-25 04:56:04 +1300

    [release script] automatically tag releases

--- release.sh
+++ release.sh
@@ -1,10 +1,19 @@
 #! /bin/sh -ea
 
+DIR=$(dirname $0)
+
+CURRENT_VERSION=$(sed -E 's/^version = "(.*)"/\1/;t;d' "$DIR/protocol/Cargo.toml")
+
 cd protocol-derive
 cargo publish
 
 cd ../protocol
 cargo publish
 
-echo "pls create and push a tag to Git"
+echo tagging $CURRENT_VERSION
+
+git tag $CURRENT_VERSION
+git push origin master $CURRENT_VERSION
+
 echo "pls update the crate versions in the README if you changed the major or minor version"
+
